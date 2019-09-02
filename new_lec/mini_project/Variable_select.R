a76cb4acc1af7a6c6d4d0d42591d623887981277
blockhead4 <blockhead4@naver.com>
2019-09-02 14:22:16 +0900

    add

--- new_lec/mini_project/Variable_select.R
+++ new_lec/mini_project/Variable_select.R
@@ -1,4 +1,4 @@
-setwd("D:/workspace-Jwp/python/DataScience/new_lec")
+setwd("D:/limworkspace/Machine-Learning/Group_Projects/Mini_project_01")
 
 data <- read.csv("data/sales_건강음료.csv")
 head(data)
@@ -25,7 +25,20 @@ summary(model)
 install.packages('leaps')
 library(leaps)
 
-leaps <- regsubsets(QTY~., data=data1, nbest=4)
+leaps <- regsubsets(QTY~., data=data1, nbest=3)
 plot(leaps, scale = 'bic')
 plot(leaps, scale = 'Cp')
 plot(leaps, scale = 'adjr2')
+
+
+# 모델 선택하기 ---------------------------------
+bic_model <- lm(QTY~ PRICE + SALEDAY + RAIN_DAY + MON, data=data1)
+summary(bic_model)
+
+Cp_model <- lm(QTY ~ ITEM_CNT + PRICE + MAXTEMP + SALEDAY + RAIN_DAY + MON, data=data1)
+summary(Cp_model)
+
+adjr2_model <- lm(QTY ~., data=data1)
+summary(adjr2_model)
+
+
